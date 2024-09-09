8c6e37036bf9750435fbbfdf08dcbdc5b369da5a
silvernuke911 <vercilbenben5@gmail.com>
2024-09-10 01:14:17 +0800

    little changes

---
+++ Q1.1.py
@@ -0,0 +1,11 @@
+# 1.1
+def fact(n):
+    p = 1
+    if n < 0 or isinstance(n, float):
+        raise ValueError('Negative or floating point numbers undefined')
+    if n == 0:
+        return 1
+    for i in range(1,n+1):
+        p*=i
+    return p
+print(fact(5.9))
